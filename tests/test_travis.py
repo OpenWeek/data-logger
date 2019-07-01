6fc5e3a2b9d674b55263f010898cec499a0af1a1
SebStreb <sebstrebelle@icloud.com>
2019-07-01 06:43:04 -0700

    working travis with selenium example

--- tests/test_travis.py
+++ tests/test_travis.py
@@ -1,4 +1,13 @@
 import pytest
+from selenium.webdriver import Firefox
+from selenium.webdriver.firefox.options import Options
 
 def test_travis():
     assert True
+
+def test_selenium():
+    options = Options()
+    options.add_argument('-headless')
+    firefox = Firefox(executable_path="./geckodriver", options=options)
+    firefox.get("http://www.example.org")
+    firefox.close()
