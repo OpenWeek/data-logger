75ca1291efdb398fe2b57d91d165081a5ae878ee
FrancoisDuchene <duchene113@gmail.com>
2019-07-02 14:15:07 +0200

    added fake data for template testing with data

--- data_logger/__init__.py
+++ data_logger/__init__.py
@@ -9,39 +9,54 @@ app = Flask(__name__)
 app.config.from_json('config.json')
 db.init_app(app)
 
+project_list = [{"id":'1',"name":'Température salle Intel'}, {"id":'22',"name":'Précipitations salle Intel'},{"id":'33' ,"name":'Précipitations salle Paul Otlet'}]
+user_name = 'Philippe'
+user_privilege = 'admin'
+basic_context = {}
+basic_context['user_name'] = user_name
+basic_context['user_privilege'] = user_privilege
+basic_context['project_list'] = project_list
+
 @app.route('/')
 def index():
-    project_list = [{"id":'1',"name":'Température salle Intel'}, {"id":'22',"name":'Température salle Intel'},{"id":'33' ,"name":'Température salle Intel'}]
-    context = {'user_name': 'Philippe', 'project_list':project_list}
+    basic_context['url'] = '/'
+    context = basic_context
     return render_template('index.html', **context)
 
 @app.route('/profile')
 def profile():
-    return profile_page(app)
+    basic_context['url'] = '/profile'
+    return profile_page(app, basic_context)
 
 @app.route('/project/<id>')
 def project(id):
-    return project_page(app, id)
+    basic_context['url'] = '/project/' + id
+    return project_page(app, basic_context, id)
 
 @app.route('/project/<id>/add/user')
 def project_add_user(id):
-    return project_add_user_page(app, id)
+    basic_context['url'] = '/project/' + id + '/add/user'
+    return project_add_user_page(app, basic_context, id)
 
 @app.route('/project/<id>/add/sensor')
 def project_add_sensor(id):
-    return project_add_sensor_page(app, id)
+    basic_context['url'] = '/project/' + id + '/add/sensor'
+    return project_add_sensor_page(app, basic_context, id)
 
 @app.route('/project/<id>/edit/user/<user_id>')
 def project_edit_user(id, user_id):
-    return project_edit_user_page(app, id, user_id)
+    basic_context['url'] = '/project/' + id + '/edit/user/' + user_id
+    return project_edit_user_page(app, basic_context, id, user_id)
 
 @app.route('/project/<id>/edit/sensor/<sensor_id>')
 def project_edit_sensor(id, sensor_id):
-    return project_edit_sensor_page(app, id, sensor_id)
+    basic_context['url'] = '/project/' + id + '/edit/sensor/' + sensor_id
+    return project_edit_sensor_page(app, basic_context, id, sensor_id)
 
 @app.route('/admin')
 def project_admin():
-    return project_admin_page(app)
+    basic_context['url'] = '/admin'
+    return project_admin_page(app, basic_context)
 
 if __name__ == "__main__":
     from flup.server.fcgi import WSGIServer
