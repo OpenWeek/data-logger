fe075ed3c6920823f27c7eeb63f743383b540672
ffran <francois.froment@student.uclouvain.be>
2019-07-02 02:43:29 -0700

    added ded db tests

--- tests/db/model_tests.py
+++ tests/db/model_tests.py
@@ -5,11 +5,6 @@ from data_logger import app
 app.app_context().push()
 db.create_all()
 
-user1 = User(name="test_user",first_name="1",email="1@1.1",admin_level=0)
-user2 = User(name="test_user",first_name="2",email="2@2.2",admin_level=0)
-user3 = User(name="test_user",first_name="3",email="3@3.3",admin_level=1)
-
-client1 = Client(state=0,enabled=True, mac="1111",ip_version="1",ip="1.1.1",added_by=user1,verified_by=user3)
 def test_User():
     user1 = User(name="test_user",first_name="1",email="1@1.1",admin_level=0)
     user2 = User(name="test_user",first_name="2",email="2@2.2",admin_level=0)
@@ -31,16 +26,24 @@ def test_User():
 def test_Client():
     user1 = User(name="test_user",first_name="1",email="1@1.1",admin_level=0)
     user3 = User(name="test_user",first_name="3",email="3@3.3",admin_level=1)
-    client1 = Client(state=0,enabled=True, mac="1111",ip_version="1",ip="1.1.1",added_by=user1,verified_by=user3)
+    firmware1 = Firmware(path="./etc",version=1,sdk="1")
+    controller1 = Controller(name="controler1",ni2c=1,nspi=1,valim=5.0,vdata=5.0)
+    client1 = Client(state=0,enabled=True, mac="1111",ip_version=1,ip="1.1.1",controller=controller1,firmware=firmware1,added_r=user1,verified_r=user3)
+
+
     try:
         db.session.add(user1)
         db.session.add(user3)
+        db.session.add(firmware1)
+        db.session.add(controller1)
         db.session.add(client1)
         db.session.commit()
         
         db.session.delete(user1)
         db.session.delete(user3)
         db.session.delete(client1)
+        db.session.delete(firmware1)
+        db.session.delete(controller1)
         db.session.commit()
         assert True
     except : 
