4cc0bc41b1585148cca38127561bcbb7928b1b39
FrancoisDuchene <duchene113@gmail.com>
2019-07-04 13:47:31 +0200

    refactor

--- data_logger/project.py
+++ data_logger/project.py
@@ -17,6 +17,20 @@ def project_page(id):
     return render_template('project.html', **context)
 """
 
+def project_page(app, basic_context, id):
+    context = basic_context
+    context['name'] = "project-"+id
+    context['id'] = id
+    context['project'] = "non"
+    # TODO: changer en vraies données de la DB
+    context['state'] = "Accepted"
+    context['data_plan'] = 'some data_plan'
+    context['verified_by'] = "Michel"
+    context['members'] = [{'name':'Philippe', 'id':'1'},{'name':'Tom', 'id': '2'}]
+    context['clients'] = [{'name':'bureau', 'id':'1'},{'name':'cafet', 'id': '2'}]
+    context['sensors_type'] = [{'sensor_name':'sol', 'id':'3', 'client_id':'2', 'sample_freq':'15', 'protocol':'QTT'},{'sensor_name':'tableau', 'id': '4', 'client_id':'1', 'sample_freq':'30', 'protocol':'QTT'}]
+    return render_template('project.html', **context)
+
 def project_add(app, basic_context):
     context = basic_context
 
