5d8b4746d5b204639f4f3c5f2639f044dbf2b659
gellens <arnaud.gellens@student.uclouvain.be>
2019-07-05 11:51:41 +0200

    add multiple periods for measurements

--- data_logger/code_client.py
+++ data_logger/code_client.py
@@ -4,9 +4,10 @@ import os
 import platform
 from subprocess import Popen, PIPE
 
+from copy import deepcopy as dc
 import nodemcu_uploader as nu
 
-DEL = False # if true remove temporary generated file
+DEL = True # if true remove temporary generated file
 
 # Utils function
 def read(file_name):
@@ -26,9 +27,12 @@ def build_flash_data(data, required_measures):
     flash_data = {}
     for sensor_id in required_measures:
         sensor_type = required_measures[sensor_id]["type"]
-        flash_data[sensor_id] = data[sensor_type]
+        sensor_period = required_measures[sensor_id]["period"]
+
+        flash_data[sensor_id] = dc(data[sensor_type])
         flash_data[sensor_id]["records"] = []
         flash_data[sensor_id]["type"] = sensor_type
+        flash_data[sensor_id]["period"] = sensor_period
 
         # build an array containing boolean value corresponding to the item in the array of the possible measurment
         for measure in data[sensor_type]["value"]:
@@ -56,6 +60,8 @@ class Code:
     def __init__(self, client_id, required_measures):
         self.template_file_main = read(self.root + "template.lua")
         self.template_file_init = read(self.root + "init_template.lua")
+        self.template_file_time = read(self.root + "time_template")
+        self.template_file_measures = read(self.root + "measures_template")
         self.data = json.loads(read(self.root + "sensors.json"))
 
         self.flash_data = build_flash_data(self.data, required_measures)
@@ -63,6 +69,8 @@ class Code:
 
         self.tmp_file_main_path = self.root + self.tmp_file_name
         self.tmp_file_init_path = self.root + "init.lua"
+        self.tmp_file_time_path = self.root + "time.json"
+        self.tmp_file_measures_path = self.root + "measures.json"
 
     def generate_code(self):
         template = Template(self.template_file_main)
@@ -72,6 +80,14 @@ class Code:
         template = Template(self.template_file_init)
         return template.render(file_name=self.tmp_file_name)
 
+    def generate_time(self):
+        template = Template(self.template_file_time)
+        return template.render(data=self.flash_data)
+    
+    def generate_measures(self):
+        template = Template(self.template_file_measures)
+        return template.render(data=self.flash_data)
+
     def write_code(self, file_path):
         write(file_path, self.generate_code())
 
@@ -79,12 +95,17 @@ class Code:
 
         self.write_code(self.tmp_file_main_path)
         write(self.tmp_file_init_path, self.generate_init())
+        write(self.tmp_file_time_path, self.generate_time())
+        write(self.tmp_file_measures_path, self.generate_measures())
+
         port = get_port()
         if port is not None:
             uploader = nu.Uploader(port=port, baud=115200)
             if uploader.prepare():
                 uploader.write_file(self.tmp_file_main_path, self.tmp_file_name, "none")
                 uploader.write_file(self.tmp_file_init_path, "init.lua", "none")
+                uploader.write_file(self.tmp_file_time_path, "time.json", "none")
+                uploader.write_file(self.tmp_file_measures_path, "measures.json", "none")
                 uploader.write_file(self.root + "json.lua", "json.lua", "none")
             else:
                 print("ERR: fatal error while preparing nodemcu for reception")
@@ -94,6 +115,8 @@ class Code:
         if DEL:
             os.remove(self.tmp_file_main_path)
             os.remove(self.tmp_file_init_path)
+            os.remove(self.tmp_file_time_path)
+            os.remove(self.tmp_file_measures_path)
 
 
 def main():
@@ -102,7 +125,13 @@ def main():
     required_measures = {
         "sensor_id": {
             "type": "bme280",
+            "period": 20, 
             "measure": ["temperature", "humidity"]
+        },
+        "sensor_id2": {
+            "type": "bme280",
+            "period": 10,
+            "measure": ["pressure"]
         }
     }
 
